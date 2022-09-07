5658fbca02d9162e85f03295ae6018712470d9ac
Mitchell Robert Vollger <mrvollger@gmail.com>
2022-09-07 10:51:55 -0700

    First template commit

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     description="m6A calling from PacBio data using ML.",
     entry_points={
         "console_scripts": [
-            "m6a-calling=m6a_calling.cli:main",
+            "m6Acalling=m6a_calling.cli:main",
         ],
     },
     install_requires=requirements,
