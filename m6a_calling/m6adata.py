505344a499e5eff689f0508f3f1ac43824990b95
Mitchell Robert Vollger <mrvollger@gmail.com>
2022-09-19 13:01:39 -0700

    Adding calls from within nucs for negatives

--- m6a_calling/m6adata.py
+++ m6a_calling/m6adata.py
@@ -290,15 +290,11 @@ def read_fiber_data(fiber_data_file):
     logging.info(f"Filtered to {len(df)} fibers")
 
     calls = pd.DataFrame(list(df.apply(extend_calls, axis=1)))
-    logging.info(f"{calls}")
     logging.info(f"Generated {calls.shape[0]} calls")
     assert len(calls) == len(df)
-
     df = pd.concat([df.reset_index(drop=True), calls.reset_index(drop=True)], axis=1)
-
     df = df[df["calls"].apply(lambda x: x.shape[0]) > 1]
     logging.info(f"Filtered to {len(df)} fibers")
-    logging.info(f"{df}")
     return df
 
 
