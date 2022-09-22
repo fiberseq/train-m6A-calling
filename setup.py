4396f9ce17810752157b678adb7aac42408baa4c
Mitchell Robert Vollger <mrvollger@gmail.com>
2022-09-21 18:58:09 -0700

    m6amldata

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         "console_scripts": [
             "m6Acalling=m6a_calling.cli:main",
             "m6adata=m6a_calling.m6adata:main",
+            "m6aMLdata=m6a_calling.m6aMLdata:main",
         ],
     },
     install_requires=requirements,
