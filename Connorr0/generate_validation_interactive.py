1bb4a75263a677a4c2162d2a17413af12b2ff4fe
Connor Finkbeiner <soccerconnor555@gmail.com>
2022-09-22 10:40:13 -0700

    Interactive scripts and envroment.yml for xgboost script

---
+++ Connorr0/generate_validation_interactive.py
@@ -0,0 +1,116 @@
+import numpy as np
+import argparse
+
+
+# this function is modified from code by Aric Preive
+def parse_arguments():
+    """
+    Parses the arguments supplied, checks everything so they are in a
+    standard format
+    """
+    # Create custom class for formatting
+    class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, 
+            argparse.RawTextHelpFormatter):
+        pass
+
+    # Create parser object
+    parser = argparse.ArgumentParser(formatter_class=CustomFormatter,
+                                     description="A script for flattening matrices for m6A")
+
+    # Split into optional and required args
+    optional = parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
+
+    # Required arguments
+    required.add_argument('-i', '--input_file',
+                          help="Path to npz file",
+                          required=True)
+
+    # Optional arguments
+    optional.add_argument('-t', '--train_file',
+                          help="Training output file name",
+                          required=False,
+                          default=None)
+    optional.add_argument('-v', '--valid_file',
+                          help="validation output file name",
+                          required=False,
+                          default=None)
+ 
+
+    # Append groups together and return
+    parser._action_groups.append(optional)
+    return parser.parse_args()
+
+
+
+
+
+def flatten(data_path,
+            train_path,
+            valid_path):
+
+    data_path_pre = data_path.split(".")[0]
+
+    # name files if not named
+    if train_path is None:
+        train_path = data_path_pre + "_train.csv"
+    if valid_path is None:
+        valid_path = data_path_pre  + "_val.csv"
+
+
+
+    # load the data
+    train_val_data = np.load(data_path, allow_pickle=True)
+
+    # Get the dictionary from the containing relevant data
+    train_val_data = train_val_data['save_data_dict'][()]
+
+    # Load training and validation features and labels
+    X_train = train_val_data['X_train']
+    y_train = train_val_data['y_train']
+    X_val = train_val_data['X_val']
+    y_val = train_val_data['y_val']
+
+
+
+
+    # Matrix subsetting
+    #X_train = X_train[:,0:4, 6:9]
+    #X_val = X_val[:,0:4, 6:9]
+
+
+    # Reshape the data
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1] * X_train.shape[2])
+    X_val = X_val.reshape(X_val.shape[0], X_val.shape[1] * X_val.shape[2])
+    
+
+
+    # Convert the labels to just 1 dimension
+    y_train = y_train[:, 0]
+    y_val = y_val[:, 0]
+    final_train_data = np.concatenate([y_train[:, np.newaxis], X_train], axis=1)
+    final_val_data= np.concatenate([y_val[:, np.newaxis], X_val], axis=1)
+
+
+    np.savetxt(train_path, final_train_data, delimiter=",")
+
+    np.savetxt(valid_path, final_val_data, delimiter=",")
+
+
+
+def main():
+    ##########################################
+    ## Prepare arguments and create folders ##
+    ##########################################
+
+    # Parse the arguments supplied
+    args = parse_arguments()
+
+
+    flatten(data_path = args.input_file,
+            train_path = args.train_file,
+            valid_path = args.valid_file)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Connorr0/m6a_XGboost_interactive.py
@@ -0,0 +1,166 @@
+#from sklearn.metrics import precision_score, recall_score, accuracy_score
+import xgboost as xgb
+import numpy as np
+import argparse
+import sklearn.metrics as metrics
+
+
+# this function is modified from code by Aric Preive
+def parse_arguments():
+    """
+    Parses the arguments supplied, checks everything so they are in a
+    standard format
+    """
+    # Create custom class for formatting
+    class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, 
+            argparse.RawTextHelpFormatter):
+        pass
+
+    # Create parser object
+    parser = argparse.ArgumentParser(formatter_class=CustomFormatter,
+                                     description="A script for running XGboost on m6A data")
+
+    # Split into optional and required args
+    optional = parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
+
+    # Required arguments
+    required.add_argument('-t', '--train_file',
+                          help="Training input file name",
+                          required=True)
+
+    required.add_argument('-v', '--valid_file',
+                          help="Validation input file name",
+                          required=True)
+
+    required.add_argument('-m', '--model_name',
+                          help="model name for saving",
+                          required=True)
+    
+
+    
+
+    # Optional arguments
+    optional.add_argument('-n', '--n_rounds_total',
+                          help="Total number of rounds to run",
+                          required=False,
+                          default=100)
+
+    optional.add_argument('-i', '--n_rounds_start',
+                          help="Number of rounds to run before comparing models",
+                          required=False,
+                          default=1)
+
+    optional.add_argument('-d', '--depth',
+                          help="depth for XgBoost",
+                          required=False,
+                          default=6)
+
+    optional.add_argument('-p', '--patience',
+                          help="number of iterations with falling AUC before stopped",
+                          required=False,
+                          default=5)
+ 
+
+    # Append groups together and return
+    parser._action_groups.append(optional)
+    return parser.parse_args()
+
+
+
+def run_XG(train_file,
+           validation_file,
+           num_round,
+           num_round_start,
+           model_name,
+           depth,
+           pat):
+
+    #print(str(num_round) + " iterations to run")
+    p = 0
+    
+    # read in data
+    dtrain = xgb.DMatrix(train_file + '?format=csv&label_column=0')
+    dtest = xgb.DMatrix(validation_file + '?format=csv&label_column=0')
+
+
+    # specify parameters via map
+    param = {'max_depth':depth, 'objective':'binary:logistic', 'eval_metric':'auc'}
+    eval_s=[(dtrain, "train"), (dtest, "test")]
+
+    # intialize model
+    bst = xgb.train(param, dtrain, num_round_start, evals=eval_s)
+
+    # "intialize" best model
+    bst.save_model('xgboost.{}_best.json'.format(model_name))
+    # "intialize" current model
+    bst.save_model('xgboost.current.json')
+
+    # save AUC
+    preds = bst.predict(dtest)
+    fpr, tpr, thresh = metrics.roc_curve(dtest.get_label(), preds, pos_label=1)
+    auc_best = metrics.auc(fpr, tpr)
+
+
+    # we already did some rounds so update num_round
+    num_round = num_round - num_round_start
+
+
+    # update the model
+    for i in range(num_round):
+        
+        # train
+        bst = xgb.train(param, dtrain, 1, evals=eval_s, xgb_model='xgboost.current.json')
+        
+        # update current model
+        bst.save_model('xgboost.current.json')
+
+        
+        # calc AUC
+        preds = bst.predict(dtest)
+        fpr, tpr, thresh = metrics.roc_curve(dtest.get_label(), preds, pos_label=1)
+        auc = metrics.auc(fpr, tpr)
+
+        if auc > auc_best:
+
+            # update auc_best
+            auc_best = auc
+
+            # save the model
+            bst.save_model('xgboost.{}_best.json'.format(model_name))
+
+            # reset patience
+            p = 0
+
+
+        else:
+            p += 1
+
+            if p >= pat:
+                break
+        
+
+
+
+
+
+def main():
+    ##########################################
+    ## Prepare arguments and create folders ##
+    ##########################################
+
+    # Parse the arguments supplied
+    args = parse_arguments()
+
+
+    run_XG(train_file = args.train_file,
+           validation_file = args.valid_file,
+           num_round = int(args.n_rounds_total),
+           num_round_start = int(args.n_rounds_start),
+           model_name = args.model_name,
+           depth = int(args.depth),
+           pat = int(args.patience))
+
+
+if __name__ == "__main__":
+    main()
